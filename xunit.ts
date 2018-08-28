7b2e31169e33c401dd66d365dd60f7b64a522cb9
Yuka Kato <satoimo.to.imo@gmail.com>
2018-08-29 01:56:01 +0900

    わからなくなったのでもう一度戻って作る

--- xunit.ts
+++ xunit.ts
@@ -1,34 +1,15 @@
-class TestCase {
-  public name: string;
-  public constructor(name: string) {
-    this.name = name;
-  }
-
-  public run() {
-    switch(this.name) {
-      case "testMethod":
-        this.testMethod();
-        break;
-    }
-  }
-}
-
-class WasRun extends TestCase {
+class WasRun {
   public wasRun: boolean;
-  public constructor(name: string) {
-    super(name);
+  public constructor(name) {
     this.wasRun = false;
   }
 
-  // wasRun フラグはこちらの子クラス特有のプロパティだが、
-  // case で testMethod を呼ぶのは親クラスのしごとになってしまっているので、
-  // 役割分担がうまくできない。
-  public testMethod() {
+  public testMethod(): void {
     this.wasRun = true;
   }
 }
 
 let test = new WasRun("testMethod");
 console.log(test.wasRun);
-test.run();
+test.testMethod();
 console.log(test.wasRun);
